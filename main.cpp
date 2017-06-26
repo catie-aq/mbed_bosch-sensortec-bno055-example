394788918e8f13ba73f4b59bb9eb9c8b0241fcb3
Pierre-Marie Ancele <pm.ancele@catie.fr>
2017-06-26 18:22:48 +0200

    fist commit

--- main.cpp
+++ main.cpp
@@ -15,19 +15,39 @@
  * limitations under the License.
  */
 #include "mbed.h"
+#include "bno055.hpp"
 
 namespace {
-#define PERIOD_MS 500
+#define PERIOD_MS 50
 }
 
 static DigitalOut led1(LED1);
+static I2C i2c(I2C1_SDA, I2C1_SCL);
+static Serial pc(SERIAL_TX, SERIAL_RX);
+
+static BNO055 bno1(&i2c);
+
+static bno055_accel_t accel;
+static bno055_euler_t euler;
+uint8_t sys, acc, gyro, mag;
 
 // main() runs in its own thread in the OS
 // (note the calls to Thread::wait below for delays)
 int main()
 {
+	pc.baud(115200);
+
+	if (bno1.initialize(BNO055::OperationMode::OperationMode_NDOF, true) != true) {
+		pc.printf("ERROR BNO055 not detected. Check your wiring and BNO I2C address\n");
+		return 0;
+	}
     while (true) {
-        printf("Alive!\n");
+    	bno1.read_accel(&accel);
+    	bno1.get_calibration_status(&sys, &acc, &gyro, &mag);
+    	bno1.read_euler(&euler);
+
+    	pc.printf("ACCEL: %.3f %.3f %.3f EULER : %.3f %.3f %.3f Calib: %d% d% d %d\n", accel.x, accel.y, accel.z,
+    			euler.x*180/3.1415, euler.y*180/3.1415, euler.z*180/3.1415, sys, acc, gyro, mag);
         led1 = !led1;
         Thread::wait(PERIOD_MS);
     }
