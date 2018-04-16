b577f2b395726732e65b585ebcd300e9d7b165d1
Pierre-Marie Ancele <pm.ancele@catie.fr>
2018-04-16 10:37:00 +0200

    update example to demonstrate fusion and non fusion mode

--- main.cpp
+++ main.cpp
@@ -28,57 +28,86 @@ void configure_sensors(void);
 static DigitalOut led1(LED1);
 static I2C i2c(I2C_SDA, I2C_SCL);
 static Serial pc(SERIAL_TX, SERIAL_RX);
+static InterruptIn button(USER_BUTTON);
 
-static BNO055 bno1(&i2c);
+static BNO055 bno(&i2c);
 
-static bno055_accelerometer_t accel;
+static bno055_linear_acceleration_t laccel;
+static bno055_acceleration_t accel;
 static bno055_euler_t euler;
-static bno055_gyroscope_t gyro1;
+static bno055_angular_velocity_t gyro;
 static bno055_calibration_status_t calib;
-static bno055_magnetometer_t mag;
+static bno055_magnetic_field_t mag;
 
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
+static EventQueue queue;
+
+static bool use_fusion = true;
+
+void button_interrupt()
+{
+	use_fusion = !use_fusion;
+	queue.call(printf, "SWITCH IMU MODE TO : %s !\n", (use_fusion? "FUSION":"NORMAL"));
+	queue.call(configure_sensors);
+}
+
+void print_sensor_data()
+{
+	if (use_fusion) {
+		laccel = bno.linear_acceleration();
+		calib = bno.calibration_status();
+		euler = bno.euler();
+		gyro = bno.angular_velocity();
+		mag = bno.magnetic_field();
+
+		printf("LINEAR ACCELERATION (m/s²): %6.3f %6.3f %6.3f  EULER (°): %6.3f %6.3f %6.3f  Calib: %d% d% d %d\n\r",
+				laccel.x, laccel.y, laccel.z, euler.x*180/3.1415, euler.y*180/3.1415, euler.z*180/3.1415, calib.system,
+				calib.accelerometer, calib.gyroscope, calib.magnetometer);
+	}
+	else {
+		accel = bno.acceleration();
+		gyro = bno.angular_velocity();
+		mag = bno.magnetic_field();
+
+        printf("ACCELERATION (m/s²): %6.3f %6.3f %6.3f  GYROSCOPE (°/s): %6.3f %6.3f %6.3f  MAGNETOMETER (µT): %6.3f %6.3f %6.3f\n\r",
+				accel.x, accel.y, accel.z, gyro.x, gyro.y, gyro.z, mag.x, mag.y, mag.z);
+	}
+
+	led1 = !led1;
+}
+
 int main()
 {
     pc.baud(115200);
 
-    if (bno1.initialize(BNO055::OperationMode::NDOF, true) != true) {
-    	printf("ERROR BNO055 not detected. Check your wiring and BNO I2C address\n\r");
+    pc.printf("6TRON BNO055 Example\n");
+    pc.printf("Press the USER BUTTON at any time to enable/disable fusion mode of the IMU\n");
+
+    if (bno.initialize(BNO055::OperationMode::NDOF, true) != true) {
+        pc.printf("ERROR BNO055 not detected. Check your wiring and BNO I2C address\n\r");
     	return 0;
     }
-    else{
-    	// if no fusion mode, configure the settings of device
-    	if(bno1.operating_mode() < BNO055::OperationMode::IMUPLUS) {
-    		// set configure device : acc, gyro and mag sensors
-    		configure_sensors();
-    	}
-    	printf("BNO055 configured\n\r");
+    else {
+        pc.printf("BNO055 configured in fusion mode !\n\r");
     }
 
-    while (true) {
-        accel = bno1.accelerometer();
-        calib = bno1.calibration_status();
-        euler = bno1.euler();
-    	gyro1 = bno1.gyroscope();
-    	mag = bno1.magnetometer();
+    button.fall(button_interrupt);
 
-    	// Orientation values are available only in fusion mode
-    	if(bno1.operating_mode() < BNO055::OperationMode::IMUPLUS) {
-    	    printf("ACCEL: %.3f %.3f %.3f GYRO : %.3f %.3f %.3f \n\r", accel.x, accel.y, accel.z,
-    	                    gyro1.x, gyro1.y, gyro1.z);
-    	} else {
-    	    printf("ACCEL: %.3f %.3f %.3f EULER : %.3f %.3f %.3f Calib: %d% d% d %d\n\r", accel.x, accel.y, accel.z,
-    			euler.x*180/3.1415, euler.y*180/3.1415, euler.z*180/3.1415, calib.system, calib.accelerometer, calib.gyroscope, calib.magnetometer);
-    	}
-        led1 = !led1;
-        Thread::wait(PERIOD_MS);
-    }
+    wait_ms(1000);
+
+    queue.call_every(PERIOD_MS, print_sensor_data);
+    queue.dispatch_forever();
 }
 
 void configure_sensors()
 {
-    bno1.set_accelerometer_configuration(BNO055::AccelerometerSensorRange::_4G, BNO055::AccelerometerSensorBandWidth::_62Hz, BNO055::AccelerometerSensorOperationMode::Normal);
-    bno1.set_gyroscope_configuration(BNO055::GyroscopeSensorRange::_2000DPS, BNO055::GyroscopeSensorBandWidth::_32Hz, BNO055::GyroscopeSensorOperationMode::Normal);
-    bno1.set_magnetometer_configuration(BNO055::MagnetometerSensorDataOutputRate::_20Hz, BNO055::MagnetometerSensorOperationMode::EnhancedRegular, BNO055::MagnetometerSensorPowerMode::ForceMode);
+	if (use_fusion) {
+		bno.set_operation_mode(BNO055::OperationMode::NDOF);
+	}
+	else {
+		bno.set_operation_mode(BNO055::OperationMode::AMG);
+
+	    bno.set_accelerometer_configuration(BNO055::AccelerometerSensorRange::_4G, BNO055::AccelerometerSensorBandWidth::_62Hz, BNO055::AccelerometerSensorOperationMode::Normal);
+	    bno.set_gyroscope_configuration(BNO055::GyroscopeSensorRange::_2000DPS, BNO055::GyroscopeSensorBandWidth::_32Hz, BNO055::GyroscopeSensorOperationMode::Normal);
+	    bno.set_magnetometer_configuration(BNO055::MagnetometerSensorDataOutputRate::_20Hz, BNO055::MagnetometerSensorOperationMode::EnhancedRegular, BNO055::MagnetometerSensorPowerMode::ForceMode);
+	}
 }
